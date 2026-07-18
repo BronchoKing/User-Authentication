d8449d76faa25701b9a283f8c6b28b267ad0f07d
Papa Kwadwo <evansobuobi977@gmail.com>
2026-07-18 19:43:49 +0000

    updated .env

--- src/Util/email.js
+++ src/Util/email.js
@@ -3,9 +3,8 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async(option) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false,
+       service: process.env.EMAIL_HOST,
+        
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
