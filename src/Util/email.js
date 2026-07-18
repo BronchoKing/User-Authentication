df3af77f20e51e8dc371e06bc4bc676c5e55da11
Papa Kwadwo <evansobuobi977@gmail.com>
2026-07-18 20:56:38 +0000

    updated email configuration

--- src/Util/email.js
+++ src/Util/email.js
@@ -1,6 +1,50 @@
 const nodemailer = require('nodemailer');
 
 
+
+const sendEmail = async (option) => {
+
+    const transporter = nodemailer.createTransport({
+
+        host: "smtp-relay.brevo.com",
+
+        port: process.env.EMAIL_PORT,
+
+        secure: false,
+
+        auth: {
+            user: process.env.BREVO_SMTP_USER,
+            pass: process.env.BREVO_SMTP_KEY
+        }
+
+    });
+
+
+    const emailOptions = {
+
+        from: "Profit Harvester Support <support@profitharvester.com>",
+
+        to: option.email,
+
+        subject: option.subject,
+
+        text: option.message
+
+    };
+
+
+    await transporter.sendMail(emailOptions);
+
+};
+
+
+module.exports = sendEmail;
+
+
+
+
+
+/*
 const sendEmail = async(option) => {
     const transporter = nodemailer.createTransport({
        service: process.env.EMAIL_HOST,
@@ -20,5 +64,7 @@ const sendEmail = async(option) => {
 
     await transporter.sendMail(emailOptions);
 }
+    */
+    
 
 module.exports = sendEmail;
