837d5e1b62ff373dd2f355813807fa6767ac5d1d
Papa Kwadwo <evansobuobi977@gmail.com>
2026-07-19 14:45:57 +0000

    updated Email.js with resend API

--- src/Util/email.js
+++ src/Util/email.js
@@ -1,6 +1,65 @@
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendEmail = async (option) => {
+  try {
+    console.log("SENDING EMAIL TO:", option.email);
+
+    const { data, error } = await resend.emails.send({
+      from: 'Profit Harvester Support <support@profitharvester.com>',
+      to: option.email,
+      subject: option.subject,
+      text: option.message,
+    });
+
+    if (error) {
+      console.log("RESEND ERROR:", error);
+      throw error;
+    }
+
+    console.log("EMAIL SENT:", data.id);
+
+    return data;
+
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
+    throw error;
+  }
+};
+
+module.exports = sendEmail;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const nodemailer = require('nodemailer');
-
-
 
 const sendEmail = async(option) => {
     try {
@@ -43,7 +102,4 @@ const sendEmail = async(option) => {
 }
 
 module.exports = sendEmail;
-
-    
-    
-
+*/
